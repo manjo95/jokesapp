b73c9965a751efcadbc080cbc320b2a5271b775b
manjo95 <selva.manoj@gmail.com>
2020-12-06 18:02:17 +0000

    Added template and ready to run the app

---
+++ src/main/java/com/example/jokesapp/jokesapp/Controllers/JokesController.java
@@ -0,0 +1,25 @@
+package com.example.jokesapp.jokesapp.Controllers;
+
+import com.example.jokesapp.jokesapp.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokesController {
+
+    private JokeService jokeService;
+
+    @Autowired
+    public JokesController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJoke(Model model){
+        model.addAttribute("joke",jokeService.getJoke());
+        return "chucknorris";
+    }
+
+}
