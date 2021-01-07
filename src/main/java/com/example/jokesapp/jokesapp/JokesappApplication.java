8139e8c067f70047d2512dd0847da07a16d45427
manjo95 <selva.manoj@gmail.com>
2021-01-07 15:21:15 +0000

    Added xml configuration to refactor the ChuckNorris class to java bean and use it in constructed injected controller.

--- src/main/java/com/example/jokesapp/jokesapp/JokesappApplication.java
+++ src/main/java/com/example/jokesapp/jokesapp/JokesappApplication.java
@@ -2,8 +2,10 @@ package com.example.jokesapp.jokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class JokesappApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/jokesapp/jokesapp/config/ChuckConfig.java
+++ src/main/java/com/example/jokesapp/jokesapp/config/ChuckConfig.java
@@ -4,9 +4,9 @@ import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class ChuckConfig {
-    @Bean
+    //@Bean
     public ChuckNorrisQuotes chuckNorrisQuotes(){
         return new ChuckNorrisQuotes();
     }
